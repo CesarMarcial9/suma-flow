dad97a26f7b28aec7b51c28aa42a4933d4522faf
Cesar <cesarmarcialq@gmail.com>
2023-01-24 11:53:11 -0800

    init style

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
-
-const DashboardPage = (props: Props) => {
+const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
-  )
-}
+    <div className="flex min-h-screen flex-col gap-8 bg-well-red-500 p-12 text-white">
+      <header>
+        <h2 className="text-4xl font-bold">Suma Wealth</h2>
+      </header>
 
-export default DashboardPage
+      <div className="flex flex-col gap-4">
+        <h2>Summary</h2>
+
+        <div className="flex items-center justify-center bg-well-red-300">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
