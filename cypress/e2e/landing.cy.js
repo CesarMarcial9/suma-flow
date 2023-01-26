e6ca539cf73a5583e701f4a665f84aac6ec56eda
Cesar <cesarmarcialq@gmail.com>
2023-01-26 12:59:26 -0800

    small testing changes

--- cypress/e2e/landing.cy.js
+++ cypress/e2e/landing.cy.js
@@ -4,7 +4,6 @@ describe("Navigation", () => {
     it('Should open the app store', () => {
         cy.visit("http://localhost:3000");
         cy.get('[data-cy="download"]').click()
-        cy.url().should("be.string")
     })
   it("Should visit dashboard", () => {
     cy.visit("http://localhost:3000");
