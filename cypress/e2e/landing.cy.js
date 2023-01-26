d425872fdc6956bf43780d9292899499530f8f0d
Cesar <cesarmarcialq@gmail.com>
2023-01-26 13:04:24 -0800

    ran prettier on cypress test and modified app store link test

--- cypress/e2e/landing.cy.js
+++ cypress/e2e/landing.cy.js
@@ -1,14 +1,12 @@
-
-
 describe("Navigation", () => {
-    it('Should open the app store', () => {
-        cy.visit("http://localhost:3000");
-        cy.get('[data-cy="download"]').click()
-    })
+  it("Should open the app store", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('[data-cy="download"]').click();
+  });
   it("Should visit dashboard", () => {
     cy.visit("http://localhost:3000");
 
-    cy.get('[data-cy="dash"]').click()
-    cy.url().should('include', '/dashboard')
+    cy.get('[data-cy="dash"]').click();
+    cy.url().should("include", "/dashboard");
   });
 });
