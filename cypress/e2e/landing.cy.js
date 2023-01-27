419204377a44946e2c15241e24455bc26fd9d0c9
Cesar <cesarmarcialq@gmail.com>
2023-01-27 11:01:51 -0800

    added about us page visit test

--- cypress/e2e/landing.cy.js
+++ cypress/e2e/landing.cy.js
@@ -3,10 +3,17 @@ describe("Navigation", () => {
     cy.visit("http://localhost:3000");
     cy.get('[data-cy="download"]').click();
   });
+
   it("Should visit dashboard", () => {
     cy.visit("http://localhost:3000");
 
     cy.get('[data-cy="dash"]').click();
     cy.url().should("include", "/dashboard");
+  });
+
+  it("Should visit about us page", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('[data-cy="about"]').click();
+    cy.url().should("include", "/about");
   });
 });
